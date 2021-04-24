2dbf9443ff7ccfb8d69c372bdd6de818511899ae
zephyrus21 <pandey.piyus@gmail.com>
2021-04-24 11:27:28 +0530

    adding styles to jumbotron

--- src/components/jumbotron/index.jsx
+++ src/components/jumbotron/index.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import {
+  Conatiner,
+  Inner,
+  Pane,
+  Title,
+  SubTitle,
+  Image,
+} from './styles/jumbotron';
 
-const Jumbotron = () => {
-  return <div>Jumbo</div>;
+const Jumbotron = ({ children, direction = 'row', ...restProps }) => {
+  return <Inner direction={direction}>{children}</Inner>;
+};
+
+Jumbotron.Conatiner = ({ children, ...restProps }) => {
+  return <Conatiner {...restProps}>{children}</Conatiner>;
+};
+Jumbotron.Pane = ({ children, ...restProps }) => {
+  return <Pane {...restProps}>{children}</Pane>;
+};
+Jumbotron.Title = ({ children, ...restProps }) => {
+  return <Title {...restProps}>{children}</Title>;
+};
+Jumbotron.SubTitle = ({ children, ...restProps }) => {
+  return <SubTitle {...restProps}>{children}</SubTitle>;
+};
+Jumbotron.Image = ({ ...restProps }) => {
+  return <Image {...restProps} />;
 };
 
 export default Jumbotron;
