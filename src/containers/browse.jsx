bf4644df255801d9d5aa39438966c5e12d14ffe2
zephyrus21 <pandey.piyus@gmail.com>
2021-04-29 12:50:01 +0530

    gg

--- src/containers/browse.jsx
+++ src/containers/browse.jsx
@@ -37,53 +37,12 @@ const BrowseContainer = ({ slides }) => {
     } else {
       setSlideRows(slides[category]);
     }
-  }, [searchTerm]);
+  }, [searchTerm, slideRows, category, slides]);
 
   return profile.displayName ? (
     <>
       {loading ? <Loading src={user.photoURL} /> : <Loading.ReleaseBody />}
-
-      <Header src='joker1' dontShowOnSmallViewPort>
-        <Header.Frame>
-          <Header.Group>
-            <Header.Logo
-              to={ROUTES.HOME}
-              src='/images/logo.svg'
-              alt='Netflix'
-            />
-            <Header.TextLink
-              active={category === 'series' ? 'true' : 'false'}
-              onClick={() => setCategory('series')}>
-              Series
-            </Header.TextLink>
-            <Header.TextLink
-              active={category === 'films' ? 'true' : 'false'}
-              onClick={() => setCategory('films')}>
-              Films
-            </Header.TextLink>
-          </Header.Group>
-          <Header.Group>
-            <Header.Search
-              searchTerm={searchTerm}
-              setSearchTerm={setSearchTerm}
-            />
-            <Header.Profile>
-              <Header.Picture src={user.photoURL} />
-              <Header.Dropdown>
-                <Header.Group>
-                  <Header.Picture src={user.photoURL} />
-                  <Header.TextLink>{user.displayName}</Header.TextLink>
-                </Header.Group>
-                <Header.Group>
-                  <Header.TextLink onClick={() => firebaseApp.auth().signOut()}>
-                    Sign out
-                  </Header.TextLink>
-                </Header.Group>
-              </Header.Dropdown>
-            </Header.Profile>
-          </Header.Group>
-        </Header.Frame>
-
+      <Header src='joker1'>
         <Header.Feature>
           <Header.FeatureCallOut>Watch Joker Now</Header.FeatureCallOut>
           <Header.Text>
@@ -96,33 +55,6 @@ const BrowseContainer = ({ slides }) => {
           <Header.PlayButton>Play</Header.PlayButton>
         </Header.Feature>
       </Header>
-
-      <Card.Group>
-        {slideRows.map((slideItem) => (
-          <Card key={`${category}-${slideItem.title.toLowerCase()}`}>
-            <Card.Title>{slideItem.title}</Card.Title>
-            <Card.Entities>
-              {slideItem.data.map((item) => (
-                <Card.Item key={item.docId} item={item}>
-                  <Card.Image
-                    src={`/images/${category}/${item.genre}/${item.slug}/small.jpg`}
-                  />
-                  <Card.Meta>
-                    <Card.SubTitle>{item.title}</Card.SubTitle>
-                    <Card.Text>{item.description}</Card.Text>
-                  </Card.Meta>
-                </Card.Item>
-              ))}
-            </Card.Entities>
-            <Card.Feature category={category}>
-              <Player>
-                <Player.Button />
-                <Player.Video src='/videos/bunny.mp4' />
-              </Player>
-            </Card.Feature>
-          </Card>
-        ))}
-      </Card.Group>
       <FooterContainer />
     </>
   ) : (
