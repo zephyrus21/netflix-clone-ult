58a5e1caee9e8713bc64c18269ee9516db08ee29
zephyrus21 <pandey.piyus@gmail.com>
2021-04-29 12:02:25 +0530

    adding filters

--- src/pages/browse.jsx
+++ src/pages/browse.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import { useContent } from '../hooks';
+import { selectionFilter } from '../utils';
 
 const Browse = () => {
+  const { series } = useContent('series');
+  const { films } = useContent('films');
+  const slides = selectionFilter({ series, films });
+
   return <div>Browse</div>;
 };
 
