7be598672000521d044795a83c5aaaa7102e580b
zephyrus21 <pandey.piyus@gmail.com>
2021-04-29 11:37:05 +0530

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,35 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Home, Browse, Signup, Signin } from './pages';
+import { IsUserRedirect, ProtectedRoute } from './helpers/routes';
 import * as ROUTES from './constants/routes';
 
 const App = () => {
+  const user = null;
   return (
     <Router>
-      <Route exact path={ROUTES.HOME}>
-        <Home />
-      </Route>
-      <Route exact path={ROUTES.BROWSE}>
-        <Browse />
-      </Route>
-      <Route exact path={ROUTES.SIGN_UP}>
-        <Signup />
-      </Route>
-      <Route exact path={ROUTES.SIGN_IN}>
+      <IsUserRedirect
+        user={user}
+        loggedInPath={ROUTES.BROWSE}
+        path={ROUTES.SIGN_IN}>
         <Signin />
-      </Route>
+      </IsUserRedirect>
+      <IsUserRedirect
+        user={user}
+        loggedInPath={ROUTES.BROWSE}
+        path={ROUTES.SIGN_UP}>
+        <Signup />
+      </IsUserRedirect>
+      <ProtectedRoute exact path={ROUTES.BROWSE} user={user}>
+        <Browse />
+      </ProtectedRoute>
+      <IsUserRedirect
+        user={user}
+        loggedInPath={ROUTES.BROWSE}
+        path={ROUTES.HOME}
+        exact>
+        <Home />
+      </IsUserRedirect>
     </Router>
   );
 };
